65dda28bff0bbe6fb81a2cf839fe81406debdaa2
Khaled Nabaheen <khalednabaheen8@gmail.com>
2023-05-22 15:41:19 +0300

    handle login button

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,18 +1,22 @@
-/* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/button-has-type */
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 
-const Navbar = () => (
-  <div className="navbar">
-    <div className="logo">
-      <img src="/images/logo-image.svg" alt="Logo" className="logo-image" />
+const Navbar = () => {
+  const loginNav = useNavigate();
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="/images/logo-image.svg" alt="Logo" className="logo-image" />
+      </div>
+      <ul className="links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+      </ul>
+      <button className="login-btn" onClick={() => loginNav('/login')}>Login</button>
     </div>
-    <ul className="links">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Products</a></li>
-    </ul>
-    <button className="login-btn">Login</button>
-  </div>
-);
+  );
+};
 
 export default Navbar;
